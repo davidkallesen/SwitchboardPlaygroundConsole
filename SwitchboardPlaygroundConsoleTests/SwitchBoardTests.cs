51dbf3578f45c41614c0f1327f3c0ae82033c752
David Kallesen <david@lmdk.dk>
2023-12-29 02:14:55 +0100

    Theory test for IsConnected

--- SwitchboardPlaygroundConsoleTests/SwitchBoardTests.cs
+++ SwitchboardPlaygroundConsoleTests/SwitchBoardTests.cs
@@ -7,8 +7,8 @@ public class SwitchBoardTests
     {
         var switchboard = new Switchboard(5, 5);
         switchboard.SetOccupied(new Point(2, 2));
-        var cell1 = switchboard.GetCell(new Point(2,2));
-        var cell2 = switchboard.GetCell(new Point(1,1));
+        var cell1 = switchboard.GetCell(new Point(2, 2));
+        var cell2 = switchboard.GetCell(new Point(1, 1));
         Assert.False(switchboard.IsConnected(cell1, cell2));
     }
 
@@ -64,4 +64,21 @@ public class SwitchBoardTests
         Assert.False(switchboard.IsConnected(cell1, cell2));
     }
     // Add more tests for each condition in the IsConnected method
+
+    [Theory]
+    [InlineData(true, 2, 2, 4, 0, 1, 1, 4, 0)]
+    [InlineData(true, 2, 2, 4, 1, 2, 1, 5, 2)]
+    [InlineData(false, 2, 2, 4, 0, 1, 1, 0, 5)]
+    [InlineData(false, 2, 2, 4, 0, 0, 0, 0, 4)]
+    public void IsConnected_LegalCases(bool expected, int p1X, int p1Y, int p1In, int p1Out, int p2X, int p2Y, int p2In, int p2Out)
+    {
+        var p1 = new Point(p1X, p1Y);
+        var p2 = new Point(p2X, p2Y);
+        var switchboard = new Switchboard(5, 5);
+        switchboard.SetInOut(p1, p1In, p1Out);
+        switchboard.SetInOut(p2, p2In, p2Out);
+        var cell1 = switchboard.GetCell(p1);
+        var cell2 = switchboard.GetCell(p2);
+        Assert.Equal(expected, switchboard.IsConnected(cell1, cell2));
+    }
 }
