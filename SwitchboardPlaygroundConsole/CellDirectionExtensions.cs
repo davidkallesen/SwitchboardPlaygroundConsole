6fa246a824ece57f92ef1b29d6eb9740326738be
Jan Krag <jan.krag@eficode.com>
2024-01-02 19:10:16 +0100

    Add Opposite() helper for CellDirection

---
+++ SwitchboardPlaygroundConsole/CellDirectionExtensions.cs
@@ -0,0 +1,9 @@
+using SwitchboardPlaygroundConsole;
+
+public static class CellDirectionExtensions
+{
+    public static CellDirection Opposite(this CellDirection direction)
+    {
+        return (CellDirection)(((int)direction + 4) % 8);
+    }
+}
