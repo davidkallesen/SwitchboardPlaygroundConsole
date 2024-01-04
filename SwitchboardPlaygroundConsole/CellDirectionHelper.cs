e7fe817349ee7ed8eff90b80cbfc9a13f71efb90
David Kallesen <david@lmdk.dk>
2024-01-04 18:29:39 +0100

    Add CellDirectionHelper

---
+++ SwitchboardPlaygroundConsole/CellDirectionHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace SwitchboardPlaygroundConsole;
+
+public static class CellDirectionHelper
+{
+    public static CellDirection GetCellDirection(char input) 
+        => (CellDirection)input - 'A';
+}
