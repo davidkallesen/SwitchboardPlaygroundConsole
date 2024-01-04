e52bf46417aa4cbcefabb5bcfd1dbd39373fa05d
David Kallesen <david@lmdk.dk>
2024-01-04 19:41:10 +0100

    WIP on SwitchboardCasesTests

---
+++ SwitchboardPlaygroundConsoleTests/SwitchboardCasesTests.cs
@@ -0,0 +1,21 @@
+﻿namespace SwitchboardPlaygroundConsoleTests;
+
+public class SwitchboardCasesTests
+{
+    private readonly ITextOutput output;
+
+    public SwitchboardCasesTests(ITestOutputHelper output)
+    {
+        this.output = new TestOutputHelperAdapter(output);
+    }
+
+    [Fact]
+    public void Hallo()
+    {
+        // TODO: var caseNames = TestCasesResourcesParser.GetCaseNames();
+
+        var switchboard = TestCasesResourcesParser.GetSwitchboardByCaseName("Case1");
+        ConsoleHelper.Render(switchboard, output);
+        Assert.True(false);
+    }
+}
