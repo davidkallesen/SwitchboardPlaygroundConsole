7f1bc1cc4acaa1ab2f54102b2c051c7f7f560709
David Kallesen <david@lmdk.dk>
2024-01-05 03:09:44 +0100

    More BenchmarkTests

--- SwitchboardCalculator.Benchmark/BenchmarkTests.cs
+++ SwitchboardCalculator.Benchmark/BenchmarkTests.cs
@@ -5,18 +5,32 @@ public class BenchmarkTests
 {
     private (Switchboard Switchboard, Point Start, Point Target) DataForTestCaseParserCase1;
     private (Switchboard Switchboard, Point Start, Point Target) DataForTestCaseParserCase2;
-    private (Switchboard Switchboard, Point Start, Point Target) DataForCaseAlreadyConnected;
-    private (Switchboard Switchboard, Point Start, Point Target) DataForCaseCircularPath;
-    private (Switchboard Switchboard, Point Start, Point Target) DataForCaseNoPossiblePath;
-    
+
+    private (Switchboard Switchboard, Point Start, Point Target) DataForFindPathBreadthCaseAlreadyConnected;
+    private (Switchboard Switchboard, Point Start, Point Target) DataForFindPathBreadthCaseCircularPath;
+    private (Switchboard Switchboard, Point Start, Point Target) DataForFindPathBreadthCaseNoPossiblePath;
+    private (Switchboard Switchboard, Point Start, Point Target) DataForFindPathBreadthCaseMaze;
+
+    private (Switchboard Switchboard, Point Start, Point Target) DataForFindPathDijkstraCaseAlreadyConnected;
+    private (Switchboard Switchboard, Point Start, Point Target) DataForFindPathDijkstraCaseCircularPath;
+    private (Switchboard Switchboard, Point Start, Point Target) DataForFindPathDijkstraCaseNoPossiblePath;
+    private (Switchboard Switchboard, Point Start, Point Target) DataForFindPathDijkstraCaseMaze;
+
     [GlobalSetup]
     public void Setup()
     {
         DataForTestCaseParserCase1 = TestCasesResourcesParser.GetSwitchboardDataByCaseName("TestCaseParser.Case1");
         DataForTestCaseParserCase2 = TestCasesResourcesParser.GetSwitchboardDataByCaseName("TestCaseParser.Case2");
-        DataForCaseAlreadyConnected = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathBreadthFirstSearch.Case_already_connected");
-        DataForCaseCircularPath = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathBreadthFirstSearch.Case_circular_path");
-        DataForCaseNoPossiblePath = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathBreadthFirstSearch.Case_no_possible_path");
+
+        DataForFindPathBreadthCaseAlreadyConnected = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathBreadthFirstSearch.Case_already_connected");
+        DataForFindPathBreadthCaseCircularPath = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathBreadthFirstSearch.Case_circular_path");
+        DataForFindPathBreadthCaseNoPossiblePath = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathBreadthFirstSearch.Case_no_possible_path");
+        DataForFindPathBreadthCaseMaze = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathBreadthFirstSearch.Case_maze");
+
+        DataForFindPathDijkstraCaseAlreadyConnected = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathDijkstra.Case_already_connected");
+        DataForFindPathDijkstraCaseCircularPath = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathDijkstra.Case_circular_path");
+        DataForFindPathDijkstraCaseNoPossiblePath = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathDijkstra.Case_no_possible_path");
+        DataForFindPathDijkstraCaseMaze = TestCasesResourcesParser.GetSwitchboardDataByCaseName("FindPathDijkstra.Case_maze");
     }
 
     [Benchmark]
@@ -38,24 +52,64 @@ public class BenchmarkTests
     [Benchmark]
     public void FindPathBreadthFirstSearch_CaseAlreadyConnected()
     {
-        DataForCaseAlreadyConnected.Switchboard.FindPathBreadthFirstSearch(
-            DataForCaseAlreadyConnected.Start,
-            DataForCaseAlreadyConnected.Target);
+        DataForFindPathBreadthCaseAlreadyConnected.Switchboard.FindPathBreadthFirstSearch(
+            DataForFindPathBreadthCaseAlreadyConnected.Start,
+            DataForFindPathBreadthCaseAlreadyConnected.Target);
     }
 
     [Benchmark]
     public void FindPathBreadthFirstSearch_CaseCircularPath()
     {
-        DataForCaseCircularPath.Switchboard.FindPathBreadthFirstSearch(
-            DataForCaseCircularPath.Start,
-            DataForCaseCircularPath.Target);
+        DataForFindPathBreadthCaseCircularPath.Switchboard.FindPathBreadthFirstSearch(
+            DataForFindPathBreadthCaseCircularPath.Start,
+            DataForFindPathBreadthCaseCircularPath.Target);
+    }
+
+    [Benchmark]
+    public void FindPathBreadthFirstSearch_CaseMaze()
+    {
+        DataForFindPathBreadthCaseMaze.Switchboard.FindPathBreadthFirstSearch(
+            DataForFindPathBreadthCaseMaze.Start,
+            DataForFindPathBreadthCaseMaze.Target);
     }
 
     [Benchmark]
     public void FindPathBreadthFirstSearch_CaseNoPossiblePath()
     {
-        DataForCaseNoPossiblePath.Switchboard.FindPathBreadthFirstSearch(
-            DataForCaseNoPossiblePath.Start,
-            DataForCaseNoPossiblePath.Target);
+        DataForFindPathBreadthCaseNoPossiblePath.Switchboard.FindPathBreadthFirstSearch(
+            DataForFindPathBreadthCaseNoPossiblePath.Start,
+            DataForFindPathBreadthCaseNoPossiblePath.Target);
+    }
+
+    [Benchmark]
+    public void FindPathDijkstra_CaseAlreadyConnected()
+    {
+        DataForFindPathDijkstraCaseAlreadyConnected.Switchboard.FindPathDijkstra(
+            DataForFindPathDijkstraCaseAlreadyConnected.Start,
+            DataForFindPathDijkstraCaseAlreadyConnected.Target);
+    }
+
+    [Benchmark]
+    public void FindPathDijkstra_CaseCircularPath()
+    {
+        DataForFindPathDijkstraCaseCircularPath.Switchboard.FindPathDijkstra(
+            DataForFindPathDijkstraCaseCircularPath.Start,
+            DataForFindPathDijkstraCaseCircularPath.Target);
+    }
+
+    [Benchmark]
+    public void FindPathDijkstra_CaseNoPossiblePath()
+    {
+        DataForFindPathDijkstraCaseNoPossiblePath.Switchboard.FindPathDijkstra(
+            DataForFindPathDijkstraCaseNoPossiblePath.Start,
+            DataForFindPathDijkstraCaseNoPossiblePath.Target);
+    }
+
+    [Benchmark]
+    public void FindPathDijkstra_CaseMaze()
+    {
+        DataForFindPathDijkstraCaseMaze.Switchboard.FindPathDijkstra(
+            DataForFindPathDijkstraCaseMaze.Start,
+            DataForFindPathDijkstraCaseMaze.Target);
     }
 }
