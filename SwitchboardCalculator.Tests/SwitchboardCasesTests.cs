13143b86ade13b5e7d978c2714bec5bb35c345ee
Jan Krag <jan.krag@eficode.com>
2024-01-05 01:59:18 +0100

    More path test cases

--- SwitchboardCalculator.Tests/SwitchboardCasesTests.cs
+++ SwitchboardCalculator.Tests/SwitchboardCasesTests.cs
@@ -29,8 +29,8 @@ public class SwitchboardCasesTests
         return Verify(output.GetOutput(), verifySettings);
     }
 
-    [Theory]
-    [MemberData(nameof(TestMemberDataForCases.FindPathBreadthFirstSearch), MemberType = typeof(TestMemberDataForCases))]
+    // [Theory]
+    // [MemberData(nameof(TestMemberDataForCases.FindPathBreadthFirstSearch), MemberType = typeof(TestMemberDataForCases))]
     public Task FindPathBreadthFirstSearch(string caseName)
     {
         // Arrange
@@ -38,6 +38,31 @@ public class SwitchboardCasesTests
         var data = TestCasesResourcesParser.GetSwitchboardDataByCaseName(caseName);
 
         var pathCells = data.Switchboard.FindPathBreadthFirstSearch(data.Start, data.Target);
+        if (pathCells.Length == 0)
+        {
+            ConsoleHelper.Render(data.Switchboard, output);
+            var s = output.GetOutput() + Environment.NewLine + "No path found";
+            return Verify(s, verifySettings);
+        }
+
+        foreach (var cell in pathCells)
+        {
+            data.Switchboard.GetCell(cell.Location).SetInOut(cell.In, cell.Out);
+        }
+
+        ConsoleHelper.Render(data.Switchboard, output);
+        return Verify(output.GetOutput(), verifySettings);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestMemberDataForCases.FindPathBreadthFirstSearch), MemberType = typeof(TestMemberDataForCases))]
+    public Task FindPathDijkstra(string caseName)
+    {
+        // Arrange
+        verifySettings.UseFileName(caseName);
+        var data = TestCasesResourcesParser.GetSwitchboardDataByCaseName(caseName);
+
+        var pathCells = data.Switchboard.FindPathDijkstra(data.Start, data.Target);
         if (pathCells.Length == 0)
         {
             ConsoleHelper.Render(data.Switchboard, output);
